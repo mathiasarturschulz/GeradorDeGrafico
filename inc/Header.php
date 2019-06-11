ba728d9b5e2df8d2fe0a023a8ec466a6dc4e238a
Mathias Artur Schulz <mathias.schulz@IFRSL.redelocal>
2019-06-11 10:09:58 -0300

    [UPD] Atualizando os arquivos iniciais

--- inc/Header.php
+++ inc/Header.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
     <title>Gerador de Gráficos</title>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
     require_once "inc/Header.php";
+    require_once "autoload.php";
+    echo "<br>";
 
-    header('Location: ViewPrincipal.class.php');
+    // header('Location: ViewPrincipal.class.php');
     
     /*
     -- Google Charts:
@@ -16,6 +18,116 @@
 
     Montar uma classe para o usuário montar um gráfico.
     */
+?>
 
+
+<div id="grafico_barra"></div>
+
+<script>
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+    var data = google.visualization.arrayToDataTable([
+        ['City', '2010 Population',],
+        ['New York City, NY', 8175000],
+        ['Los Angeles, CA', 3792000],
+        ['Chicago, IL', 2695000],
+        ['Houston, TX', 2099000],
+        ['Philadelphia, PA', 1526000]
+    ]);
+
+    var options = {
+        title: 'Population of Largest U.S. Cities',
+        chartArea: {
+            width: '50%'
+        },
+        hAxis: {
+            title: 'Total Population',
+            minValue: 0
+        },
+        vAxis: {
+            title: 'City'
+        }
+    };
+
+    var chart = new google.visualization.BarChart(document.getElementById('grafico_barra'));
+
+    chart.draw(data, options);
+}
+</script>
+
+
+<div id="grafico_linha"></div>
+
+<script>
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Dogs');
+
+    data.addRows([
+        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+        [66, 70], [67, 72], [68, 75], [69, 80]
+    ]);
+
+    var options = {
+        title: 'Dogs',
+        hAxis: {
+            title: 'Time'
+        },
+        vAxis: {
+            title: 'Popularity'
+        }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('grafico_linha'));
+
+    chart.draw(data, options);
+}
+</script>
+
+
+<div id="grafico_pizza" style="width: 900px; height: 500px;"></div>
+
+<script>
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Watch TV', 2],
+        ['Sleep',    7]
+    ]);
+
+    var options = {
+        title: 'My Daily Activities'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('grafico_pizza'));
+
+    chart.draw(data, options);
+}
+</script>
+
+<?php
     require_once "inc/Footer.php";
 ?>
