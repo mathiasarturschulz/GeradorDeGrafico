f99795af1535890d7c884edb19b6e0098afd1fa8
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2019-06-16 13:17:25 -0300

    [UPD] Apagando arquivos desnecessários e atualizando o index.php

--- index.php
+++ index.php
@@ -61,39 +61,6 @@
 <div id="grafico_pizza" style="width: 900px; height: 500px;"></div>
 <br><br>
 
-
-
-<!--
-<div id="grafico_pizza" style="width: 900px; height: 500px;"></div>
-
-<script>
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-
-function drawChart() {
-
-    //titulo, arrayNomeValor
-
-    var data = google.visualization.arrayToDataTable([
-        ['Task', 'Hours per Day'],
-        ['Work',     11],
-        ['Eat',      2],
-        ['Commute',  2],
-        ['Watch TV', 2],
-        ['Sleep',    7]
-    ]);
-
-    var options = {
-        title: 'My Daily Activities'
-    };
-
-    var chart = new google.visualization.PieChart(document.getElementById('grafico_pizza'));
-
-    chart.draw(data, options);
-}
-</script>
--->
-
 <?php
     require_once "inc/Footer.php";
 ?>
